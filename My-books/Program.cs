aba40c93e2e1b231f69a4e68e075ca03c747f18c
JmlKhan <jmlkhon17@gmail.com>
2021-12-27 13:30:34 +0500

    setting up serilog

--- My-books/Program.cs
+++ My-books/Program.cs
@@ -4,11 +4,18 @@ using Microsoft.EntityFrameworkCore;
 using My_books.Data;
 using My_books.Data.Services;
 using My_books.Exceptions;
+using Serilog;
+
+Log.Logger = new LoggerConfiguration().CreateLogger();
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Host.UseSerilog();
+
 // Add services to the container.
 builder.Services.AddControllers();
+
+Log.CloseAndFlush();
 
 //db setting
 builder.Services.AddDbContext<AppDbContext>(options => 
@@ -32,10 +39,12 @@ builder.Services.AddApiVersioning(config =>
     config.ApiVersionReader = new HeaderApiVersionReader("custom-version-header");
 });
 
+
 builder.Services.AddSwaggerGen();
 
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -43,6 +52,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
 
 app.UseHttpsRedirection();
 
