71accb79a2c8d29ad4ecff9fff2e370bf7e47da4
JmlKhan <jmlkhon17@gmail.com>
2022-02-21 13:24:47 +0500

    set up Nunit and inMemory package

--- My-books/Controllers/PublishersController.cs
+++ My-books/Controllers/PublishersController.cs
@@ -21,6 +21,13 @@ namespace My_books.Controllers
             _logger = logger;
         }
 
+        [HttpGet("test")]
+        public IActionResult test()
+        {
+            var test = "hellooo";
+            return Ok(test);
+        }
+
         [HttpGet("get-all-publishers")]
         public IActionResult GetAllPublishers(string? sortBy ,string? searchString, int pageNumber)
         {
---
+++ My-books/obj/Release/net6.0/PubTmp/EFSQLScripts/My_books.Data.AppDbContext.sql
@@ -0,0 +1,210 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20211224104001_InitialDbMigration')
+BEGIN
+    CREATE TABLE [Books] (
+        [Id] int NOT NULL IDENTITY,
+        [Title] nvarchar(max) NOT NULL,
+        [Description] nvarchar(max) NOT NULL,
+        [isRead] bit NOT NULL,
+        [DateRead] datetime2 NULL,
+        [Rate] int NULL,
+        [Genre] nvarchar(max) NOT NULL,
+        [Author] nvarchar(max) NOT NULL,
+        [CoverUrl] nvarchar(max) NOT NULL,
+        [DateAdded] datetime2 NOT NULL,
+        CONSTRAINT [PK_Books] PRIMARY KEY ([Id])
+    );
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20211224104001_InitialDbMigration')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20211224104001_InitialDbMigration', N'6.0.1');
+END;
+GO
+
+COMMIT;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20211225043305_PublisherAdded')
+BEGIN
+    DECLARE @var0 sysname;
+    SELECT @var0 = [d].[name]
+    FROM [sys].[default_constraints] [d]
+    INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+    WHERE ([d].[parent_object_id] = OBJECT_ID(N'[Books]') AND [c].[name] = N'Author');
+    IF @var0 IS NOT NULL EXEC(N'ALTER TABLE [Books] DROP CONSTRAINT [' + @var0 + '];');
+    ALTER TABLE [Books] ALTER COLUMN [Author] nvarchar(max) NULL;
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20211225043305_PublisherAdded')
+BEGIN
+    ALTER TABLE [Books] ADD [PublisherId] int NOT NULL DEFAULT 0;
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20211225043305_PublisherAdded')
+BEGIN
+    CREATE TABLE [Publisher] (
+        [Id] int NOT NULL IDENTITY,
+        [Name] nvarchar(max) NOT NULL,
+        CONSTRAINT [PK_Publisher] PRIMARY KEY ([Id])
+    );
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20211225043305_PublisherAdded')
+BEGIN
+    CREATE INDEX [IX_Books_PublisherId] ON [Books] ([PublisherId]);
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20211225043305_PublisherAdded')
+BEGIN
+    ALTER TABLE [Books] ADD CONSTRAINT [FK_Books_Publisher_PublisherId] FOREIGN KEY ([PublisherId]) REFERENCES [Publisher] ([Id]) ON DELETE CASCADE;
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20211225043305_PublisherAdded')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20211225043305_PublisherAdded', N'6.0.1');
+END;
+GO
+
+COMMIT;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20211225054335_ManyToManyAdded')
+BEGIN
+    ALTER TABLE [Books] DROP CONSTRAINT [FK_Books_Publisher_PublisherId];
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20211225054335_ManyToManyAdded')
+BEGIN
+    ALTER TABLE [Publisher] DROP CONSTRAINT [PK_Publisher];
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20211225054335_ManyToManyAdded')
+BEGIN
+    EXEC sp_rename N'[Publisher]', N'Publishers';
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20211225054335_ManyToManyAdded')
+BEGIN
+    ALTER TABLE [Publishers] ADD CONSTRAINT [PK_Publishers] PRIMARY KEY ([Id]);
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20211225054335_ManyToManyAdded')
+BEGIN
+    CREATE TABLE [Authors] (
+        [Id] int NOT NULL IDENTITY,
+        [FullName] nvarchar(max) NOT NULL,
+        CONSTRAINT [PK_Authors] PRIMARY KEY ([Id])
+    );
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20211225054335_ManyToManyAdded')
+BEGIN
+    CREATE TABLE [Book_Authors] (
+        [Id] int NOT NULL IDENTITY,
+        [BookId] int NOT NULL,
+        [AuthorId] int NOT NULL,
+        CONSTRAINT [PK_Book_Authors] PRIMARY KEY ([Id]),
+        CONSTRAINT [FK_Book_Authors_Authors_AuthorId] FOREIGN KEY ([AuthorId]) REFERENCES [Authors] ([Id]) ON DELETE CASCADE,
+        CONSTRAINT [FK_Book_Authors_Books_BookId] FOREIGN KEY ([BookId]) REFERENCES [Books] ([Id]) ON DELETE CASCADE
+    );
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20211225054335_ManyToManyAdded')
+BEGIN
+    CREATE INDEX [IX_Book_Authors_AuthorId] ON [Book_Authors] ([AuthorId]);
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20211225054335_ManyToManyAdded')
+BEGIN
+    CREATE INDEX [IX_Book_Authors_BookId] ON [Book_Authors] ([BookId]);
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20211225054335_ManyToManyAdded')
+BEGIN
+    ALTER TABLE [Books] ADD CONSTRAINT [FK_Books_Publishers_PublisherId] FOREIGN KEY ([PublisherId]) REFERENCES [Publishers] ([Id]) ON DELETE CASCADE;
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20211225054335_ManyToManyAdded')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20211225054335_ManyToManyAdded', N'6.0.1');
+END;
+GO
+
+COMMIT;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20211225123424_BookAuthorColumnRemoved')
+BEGIN
+    DECLARE @var1 sysname;
+    SELECT @var1 = [d].[name]
+    FROM [sys].[default_constraints] [d]
+    INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+    WHERE ([d].[parent_object_id] = OBJECT_ID(N'[Books]') AND [c].[name] = N'Author');
+    IF @var1 IS NOT NULL EXEC(N'ALTER TABLE [Books] DROP CONSTRAINT [' + @var1 + '];');
+    ALTER TABLE [Books] DROP COLUMN [Author];
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20211225123424_BookAuthorColumnRemoved')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20211225123424_BookAuthorColumnRemoved', N'6.0.1');
+END;
+GO
+
+COMMIT;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20220101113053_Jamol')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20220101113053_Jamol', N'6.0.1');
+END;
+GO
+
+COMMIT;
+GO
+
